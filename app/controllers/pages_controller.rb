85f16d2a18de432cfc271d8b7c9d424563af03cd
Andre Oliveira <andrehmo@hotmail.com>
2025-06-06 16:13:13 +0200

    Heroku authentication remove before_action again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
+    if defined?(authenticate_user!)
+      skip_before_action :authenticate_user!, only: [:home]
+    end
+
   def home
   end
 end
